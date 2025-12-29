8b31a37eb636e87c95fcff9dfca210974c6200b3
sayed725 <ssayed72533@gmail.com>
2025-12-29 20:11:58 +0600

    added additional feilds

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -7,4 +7,29 @@ export const auth = betterAuth({
     database: prismaAdapter(prisma, {
         provider: "postgresql", // or "mysql", "postgresql", ...etc
     }),
+
+    user: {
+       additionalFields: {
+        role: {
+            type: "string",
+            defaultValue: "USER",
+            required: false
+        },
+        phone: {
+            type: "string",
+            required: false
+        },
+        status: {
+            type: "string",
+            defaultValue: "ACTIVE",
+            required: false
+
+        }
+       }
+    },
+
+
+    emailAndPassword: { 
+    enabled: true, 
+    },
 });
